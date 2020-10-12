712b588df550ddf4ede2af021da6aeb1e9d94ac5
MajorProject2018 <36651421+MajorProject2018@users.noreply.github.com>
2020-10-12 18:26:19 -0400

    Add files via upload

---
+++ Controller.php
@@ -0,0 +1,18 @@
+<?php 
+    class Controller
+    {
+        protected $model = null;
+        protected $view = null;
+
+        public function setModel(Model $m): $model 
+        {
+
+        }
+
+        public function setView(View $v): $view
+        {
+
+        }
+    } 
+
+?>
